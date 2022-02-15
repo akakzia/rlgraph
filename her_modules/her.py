b42523bfeb1f5ff534857ff3149cfe80c6849e53
ahmed <ahmed.akakzia@gmail.com>
2022-02-15 19:14:21 +0100

    Implement reward that first takes into consideration base cubes

--- her_modules/her.py
+++ her_modules/her.py
@@ -1,7 +1,65 @@
 import numpy as np
 from scipy.linalg import block_diag
 from utils import get_idxs_per_relation, get_idxs_per_object
+from collections import deque
+from itertools import combinations, permutations
 
+g_structure = list(combinations(np.arange(5), 2)) + list(permutations(np.arange(5), 2)) 
+
+def get_ordered(goal, map):
+  i_stacks = 10
+  res = [] # will contain deques for different stacks
+  for i in range(i_stacks, len(map)):
+    base = g_structure[i][1]
+    summit = g_structure[i][0]
+    if goal[i] == 1.:
+      if len(res) == 0:
+        res.append(deque([base, summit]))
+      else:
+        j = 0
+        added = False
+        while not added and j < len(res):
+          current_stack = res[j]
+          base_current_stack = current_stack[0]
+          summit_current_stack = current_stack[-1]
+          if base == summit_current_stack:
+            res[j].append(summit)
+            added = True
+            j = 0
+          elif summit == base_current_stack:
+            res[j].appendleft(base)
+            added = True
+            j = 0 
+          j = j + 1
+        if j == len(res):
+          res.append(deque([base, summit]))
+  finished = False
+  flattened_res = res
+  try:
+    while len(flattened_res) > 1 and not finished:
+        for i in range(len(flattened_res)-1):
+                j = i
+                while j < len(flattened_res):
+                    j = j + 1
+                    if flattened_res[j][0] == flattened_res[i][-1]:
+                        flattened_res[i].pop()
+                        flattened_res[i] = flattened_res[i] + flattened_res[j]
+                        flattened_res.remove(flattened_res[j])
+                        finished = False
+                        if len(flattened_res) == 1:
+                            return flattened_res
+                    elif flattened_res[j][-1] == flattened_res[i][0]:
+                        flattened_res[j].pop()
+                        flattened_res[j] = flattened_res[j] + flattened_res[i]
+                        flattened_res.remove(flattened_res[i])
+                        finished = False
+                        if len(flattened_res) == 1:
+                            return flattened_res
+                    else:
+                        finished = True
+    return flattened_res
+  except IndexError:
+      return []
 
 class her_sampler:
     def __init__(self, args, reward_func=None):
@@ -87,13 +145,31 @@ class her_sampler:
         return transitions
 
     def compute_reward_masks(self, ag, g):
-        if self.reward_type == 'sparse':
-            return (ag == g).all().astype(np.float32)
-        elif self.reward_type == 'per_predicate':
-            return (ag == g).astype(np.float32).sum()
+        ordered_piles = get_ordered(g, g_structure)
+        if len(ordered_piles) == 0:
+            if self.reward_type == 'sparse':
+                return (ag == g).all().astype(np.float32)
+            elif self.reward_type == 'per_predicate':
+                return (ag == g).astype(np.float32).sum()
+            else:
+                reward = 0.
+                for subgoal in self.semantic_ids:
+                    if (ag[subgoal] == g[subgoal]).all():
+                        reward = reward + 1.
         else:
+            # Test new reward ordered by piles
             reward = 0.
-            for subgoal in self.semantic_ids:
-                if (ag[subgoal] == g[subgoal]).all():
-                    reward = reward + 1.
+            treated_objects = []
+            for pile in ordered_piles:
+                for i in pile:
+                    if (self.semantic_ids[i] == self.semantic_ids[i]).all() and i not in treated_objects:
+                        reward = reward + 1.
+                        treated_objects.append(i)
+                    elif (self.semantic_ids[i] != self.semantic_ids[i]).all():
+                        return reward
+            for i, subgoal in enumerate(self.semantic_ids):
+                if i not in treated_objects:
+                    if (ag[subgoal] == g[subgoal]).all():
+                        reward = reward + 1.
+
         return reward
